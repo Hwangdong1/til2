7bad120ed603fa9da5f1a8824e1772c887921f60
Hwangdong1 <ehdgus0315@naver.com>
2023-09-22 23:57:08 +0900

    0922

--- java.js
+++ java.js
@@ -1,1 +1,29 @@
-console.log(alert);
+while(true){
+    let answer = confim();
+    if(!answer){
+        break;
+    }
+}
+
+//for문
+
+for (let i =0; i<10; i++){
+    //  반복코드
+}
+
+//while 문
+git
+let i =0
+
+while(i<10){
+    //코드 
+    console.log(i);
+    i++;
+}
+
+//do.. while 문
+let i = 0;
+do{
+    //코드 
+    i==;
+}while (i<10)
