7edc31635e28e22a471527f329f1ea96a5d908e6
Hwangdong1 <ehdgus0315@naver.com>
2023-09-23 20:01:17 +0900

    0923

--- java.js
+++ java.js
@@ -5,6 +5,7 @@ while(true){
     }
 }
 
+//반복문 
 //for문
 
 for (let i =0; i<10; i++){
@@ -12,7 +13,6 @@ for (let i =0; i<10; i++){
 }
 
 //while 문
-git
 let i =0
 
 while(i<10){
@@ -27,3 +27,36 @@ do{
     //코드 
     i==;
 }while (i<10)
+
+//break
+
+while(true){
+answer = confirm("");
+if(!answer){
+    break;
+ }
+}
+
+//continue
+for(let i = 0; i<10; i++){
+    if(i%2){
+        continue;
+    }
+    console.log(i)
+}//2의 배수찍는 문장
+
+//switch문
+let fruit = prompt('티켓을 고르시오')
+
+switch (ticket){
+    case '스탠딩 sr석';
+     console.log ('200000');
+     break;
+    case '스탠딩 a석';
+     console.log ('170000');
+     break;
+    case '스탠딩 b석';
+    console.log ('150000');
+    break;
+}
+
